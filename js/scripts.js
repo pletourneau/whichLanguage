6e184ee29d08d58c619f24f5ce6c9e33746adde2
Paul LeTourneau <pletourneau62@gmail.com>
2023-08-11 09:32:43 -0700

    add script basic functionality

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function handleSubmission(event) {
+  event.preventDefault();
+
+
+  const q1 = document.querySelector("input[name='q1']:checked").value;
+  const q2 = document.querySelector("input[name='q2']:checked").value;
+  const q3 = document.querySelector("input[name='q3']:checked").value;
+  const q4 = document.querySelector("input[name='q4']:checked").value;
+  const q5 = document.querySelector("input[name='q5']:checked").value;
+  // let abilities = document.querySelectorAll("input[name='ability']:checked");
+  console.log(operator);
+
+  
+  // let result = operator + "with" + values;
+  document.getElementById("output").innerText = result;
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("char");
+  form.addEventListener("submit", handleSubmission);
+  
+});
